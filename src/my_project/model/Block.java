d56d0faa517960cf5a43f1122bf2a3944ee143dd
Bunterninja2609 <121108968+Bunterninja2609@users.noreply.github.com>
2025-05-09 10:25:21 +0200

    Update Block.java to draw Blocks

--- src/my_project/model/Block.java
+++ src/my_project/model/Block.java
@@ -3,4 +3,8 @@ package my_project.model;
 import KAGO_framework.model.GraphicalObject;
 
 public class Block extends GraphicalObject {
+	@Override
+	public void draw(Drawtool drawtool){
+		drawtool.drawImage(x, y, getImage())
+	}
 }
