5b9a322780d4e8423fbf2471761a1aaa08ba31d9
Bunterninja2609 <121108968+Bunterninja2609@users.noreply.github.com>
2025-05-09 13:52:48 +0200

    Grundlegende Struktur Block side (nicht funktionsfähig)

---
+++ src/my_project/Picture.java
@@ -0,0 +1,7 @@
+package my_project;
+
+import KAGO_framework.model.GraphicalObject;
+
+public class Picture extends GraphicalObject {
+
+}
---
+++ src/my_project/model/blocks/Dirt.java
@@ -0,0 +1,5 @@
+package my_project.model.blocks;
+
+public class Dirt {
+
+}
