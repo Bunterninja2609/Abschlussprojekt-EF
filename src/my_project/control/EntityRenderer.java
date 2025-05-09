7f57e53f14ea6163089d41d648d50e8d931bc52e
Tom0le <schutom@kkg-do.eu>
2025-05-09 14:01:24 +0200

    Entity Angefangen: 09.05. 14:00

--- src/my_project/control/EntityRenderer.java
+++ src/my_project/control/EntityRenderer.java
@@ -1,4 +1,25 @@
 package my_project.control;
 
+import KAGO_framework.view.DrawTool;
+import my_project.model.Entity;
+
+import java.util.ArrayList;
+
 public class EntityRenderer {
+
+    private ArrayList<Entity> entities;
+
+    public EntityRenderer(){
+
+    }
+
+    //@Override
+    public void draw(DrawTool drawTool) {
+
+    }
+
+    //@Override
+    public void update(double dt) {
+
+    }
 }
