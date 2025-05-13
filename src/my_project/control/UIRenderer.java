2c79bd75e83b66a714ee474075acf9c4d5617dbb
Tom0le <schutom@kkg-do.eu>
2025-05-13 13:13:14 +0200

    UI fps

--- src/my_project/control/UIRenderer.java
+++ src/my_project/control/UIRenderer.java
@@ -2,7 +2,19 @@ package my_project.control;
 
 import KAGO_framework.view.DrawTool;
 
+import java.awt.*;
+
 public class UIRenderer {
+
+    private int fps = 0;
+
     public UIRenderer() {}
-    public void draw(DrawTool drawTool) {}
+
+    public void draw(DrawTool drawTool) {
+        drawTool.setCurrentColor(new Color(0,0,0));
+        drawTool.drawText(10,10, String.valueOf(fps));
+    }
+    public void update(double dt) {
+        fps = (int) Math.floor(1/dt);
+    }
 }
