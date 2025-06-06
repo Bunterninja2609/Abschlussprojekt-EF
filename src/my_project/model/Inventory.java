2dc269a51c0d4b20d659c687b6928942fc609c78
Tom0le <schutom@kkg-do.eu>
2025-06-06 14:22:25 +0200

    inv

--- src/my_project/model/Inventory.java
+++ src/my_project/model/Inventory.java
@@ -1,16 +1,33 @@
 package my_project.model;
 
-public class Inventory {
+import KAGO_framework.model.GraphicalObject;
+import KAGO_framework.view.DrawTool;
 
-    private int size;
-    private Item[] items;
+import java.awt.*;
+
+public class Inventory extends GraphicalObject {
+
+    public int size;
+    public Item[] items;
 
     public Inventory(int size) {
         this.size = size;
         items = new Item[size];
     }
 
+    @Override
+    public void draw(DrawTool drawTool) {
+        for (int i = 0; i < size; i++) {
+            drawTool.setCurrentColor(new Color(131, 131, 131, 100));
+            drawTool.drawFilledRectangle(20*i,20,50,50);
+        }
+    }
+
     public void addItem(int slot){
+
+    }
+
+    public void removeItem(int slot){
 
     }
 
