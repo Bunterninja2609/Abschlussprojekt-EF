701b4994b80c2d2f45c068f364cf03bc2b933765
Proman <Proman@anymail.com>
2025-06-23 19:45:55 +0200

    endscreen bisschen anders

--- src/my_project/uI/EndScreen.java
+++ src/my_project/uI/EndScreen.java
@@ -17,13 +17,12 @@ public class EndScreen extends GraphicalObject {
 
     protected int screenWidth;
     protected int screenHeight;
-    protected boolean visible;
 
     protected RespawnButton respawnButton;
     protected MenuButton menuButton;
     private Texture texture;
 
-    private final int[][] stars;
+    private final int[][] blood;
     private final Random random = new Random();
     private double xOffset = 0;
 
@@ -32,30 +31,24 @@ public class EndScreen extends GraphicalObject {
     public EndScreen(int screenWidth, int screenHeight, String texturePath) {
 
         int numberOfStars = 160;
-        stars = new int[numberOfStars][3];
+        blood = new int[numberOfStars][3];
         for (int i = 0; i < numberOfStars; i++) {
-            stars[i][0] = random.nextInt(screenWidth);   // x
-            stars[i][1] = random.nextInt(screenHeight);  // y
-            stars[i][2] = 1 + random.nextInt(6);   // radius
+            blood[i][0] = random.nextInt(screenWidth);   // x
+            blood[i][1] = random.nextInt(screenHeight);  // y
+            blood[i][2] = 1 + random.nextInt(6);   // radius
         }
         texture = new Texture(texturePath);
 
 
         this.screenWidth = screenWidth;
         this.screenHeight = screenHeight;
-        this.visible = false;
 
         int buttonWidth = 200;
         int buttonHeight = 50;
         int centerX = screenWidth/2 - buttonWidth/2;
 
-        //respawnButton = new RespawnButton(centerX, screenHeight / 2, buttonWidth, buttonHeight, false, 1);
-        //menuButton = new MenuButton(centerX , screenHeight / 2 + 70, buttonWidth, buttonHeight, false, 2);
     }
 
-    public void setVisible(boolean visible) {
-        //Muss noch ergänzt werden, wenn der spieler stirbt blablabla
-    }
 
     @Override
     public void draw(DrawTool drawTool){
@@ -65,10 +58,10 @@ public class EndScreen extends GraphicalObject {
         if (Renderer.getSCENE() == 3) {
 
             drawTool.setCurrentColor(Color.RED);
-            for (int[] star : stars) {
-                int x = (int) ((star[0] + xOffset) % screenWidth);
-                int y = star[1];
-                drawTool.drawFilledCircle(x, y, star[2]);
+            for (int[] blut : blood) {
+                int x = (int) ((blut[0] + xOffset) % screenWidth);
+                int y = blut[1];
+                drawTool.drawFilledCircle(x, y, blut[2]);
             }
 
 
@@ -76,16 +69,14 @@ public class EndScreen extends GraphicalObject {
 
             drawTool.setCurrentColor(Color.RED);
             drawTool.formatText("monospaced",3,45);
-            drawTool.drawText(screenWidth / 2 - 140, screenHeight / 2 - 60, "Game Over");
+            drawTool.drawText(screenWidth / 2 - 160, screenHeight / 2 - 60, "Game Over");
 
             startButton02.draw(drawTool);
-            //respawnButton.draw(drawTool);
-            //menuButton.draw(drawTool);
         }
     }
 
 
-    //keine ahnung wie ich mouse event mache
+
     public void update(double dt) {
         if (Renderer.getSCENE() == 3) {
 
@@ -93,11 +84,7 @@ public class EndScreen extends GraphicalObject {
             if (xOffset > screenWidth) {
                 xOffset = 0;
             }
-
-
             startButton02.update(dt);
-            //respawnButton.update(dt);
-            //menuButton.update(dt);
         }
     }
 }
